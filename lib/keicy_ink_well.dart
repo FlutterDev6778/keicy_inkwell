9de47ca67097c487477890dd5d0e70c1ac915a9b
FlutterDev <Keicy6778@gmail.com>
2020-09-08 22:54:44 +0100

    2020-09-08
    Signed-off-by: FlutterDev <Keicy6778@gmail.com>

---
+++ lib/keicy_ink_well.dart
@@ -0,0 +1,28 @@
+library keicy_ink_well;
+
+import 'package:flutter/material.dart';
+
+class KeicyInkWell extends StatelessWidget {
+  final VoidCallback onTap;
+  final Widget child;
+  final bool unfocusable;
+
+  const KeicyInkWell({Key key, this.onTap, @required this.child, this.unfocusable = false}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      hoverColor: Colors.transparent,
+      focusColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      onTap: () {
+        if (unfocusable) {
+          FocusScope.of(context).requestFocus(new FocusNode());
+        }
+        onTap();
+      },
+      child: child,
+    );
+  }
+}
